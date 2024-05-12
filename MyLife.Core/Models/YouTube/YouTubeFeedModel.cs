9d0474e344636c9fe88483ccde794504ec948fd6
Tobias Scholze <tobias.scholze@outlook.com>
2024-05-12 12:18:04 +0200

    [Core] First steps to add a YouTube Service

---
+++ MyLife.Core/Models/YouTube/YouTubeFeedModel.cs
@@ -0,0 +1,33 @@
+﻿namespace MyLife.Core.Models.YouTube
+{
+    /// <summary>
+    /// Describes a YouTube rss feed.
+    /// </summary>
+    public class YouTubeChannelModel
+    {
+        /// <summary>
+        /// Title of the channel.
+        /// </summary>
+        public required string Title { get; set; }
+
+        /// <summary>
+        /// Description of the channel.
+        /// </summary>
+        public required string Description { get; set; }
+
+        /// <summary>
+        /// Link to the channel.
+        /// </summary>
+        public required string Link { get; set; }
+
+        /// <summary>
+        /// Author'S name of the channel.
+        /// </summary>
+        public required string Author { get; set; }
+
+        /// <summary>
+        /// Thumbnail url of the channel.
+        /// </summary>
+        public required string ThumbnailUrl { get; set; }
+    }
+}
---
+++ MyLife.Core/Services/YouTubeService.cs
@@ -0,0 +1,22 @@
+﻿namespace MyLife.Core.Services
+{
+    public class YouTubeService
+    {
+        // Create a function that loads the rss feed of a youtube channel by its name
+        public async Task<string> LoadRssFeedAsync(string channelName)
+        {
+            // Create a new HttpClient
+            var client = new HttpClient();
+
+            // Load the rss feed of the channel
+            var response = await client.GetAsync($"https://www.youtube.com/feeds/videos.xml?channel_id={channelName}");
+
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+
+
+    // Create a model from a youtube rss feed
+
+}
