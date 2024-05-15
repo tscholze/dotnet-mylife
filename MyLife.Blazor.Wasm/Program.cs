cf4850440747a37b42556031a18c7063048c4dc6
Tobias Scholze <tobias.scholze@outlook.com>
2024-05-15 17:06:25 +0200

    [WASM] Made life fetching async

--- MyLife.Blazor.Wasm/Program.cs
+++ MyLife.Blazor.Wasm/Program.cs
@@ -3,12 +3,18 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using MyLife.Blazor.Wasm;
 using MyLife.Core.Services;
 
+
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddSingleton<LifeService>();
-builder.Services.AddSingleton<MediumService>();
+Console.WriteLine(builder.HostEnvironment.BaseAddress);
+var httpClient = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
+var lifeService = new LifeService(httpClient);
+var mediumService = new MediumService(httpClient);
+
+builder.Services.AddScoped(sp => httpClient);
+builder.Services.AddSingleton(lifeService);
+builder.Services.AddSingleton(mediumService);
 
 await builder.Build().RunAsync();
