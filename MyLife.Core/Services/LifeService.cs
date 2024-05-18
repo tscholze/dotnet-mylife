89ebccb0394eb962635e90c6eb0e5edbe54d9e1a
Tobias Scholze <tobias.scholze@outlook.com>
2024-05-18 19:33:31 +0200

    [Core] Added cc-publications support to LifeService

--- MyLife.Core/Services/LifeService.cs
+++ MyLife.Core/Services/LifeService.cs
@@ -1,19 +1,31 @@
 ﻿using MyLife.Core.Generator;
 using MyLife.Core.Models;
+using MyLife.Core.Models.ContentCreation;
 using Newtonsoft.Json;
 
 namespace MyLife.Core.Services
 {
     /// <summary>
     /// Represents a service to load life information.
+    /// Useable in a WASM-based context.
     /// </summary>
-    /// <param name="httpClient">The HttpClient to use for fetching the life information.</param>
-    /// <param name="useRemote">If true, the life.jsopn from remote /wwwroot will be used. Elsewise sample data.</param>
-    public class LifeService(HttpClient httpClient, bool useRemote = true)
+    /// <param name="httpClient">The [HttpClient] to use for fetching the life information.</param>
+    /// <param name="mocked">If true, no remote data will be used. It will be replaced by SampleGenerator data.</param>
+    public class LifeService(HttpClient httpClient, bool mocked = false)
     {
         #region Private member
 
+        /// <summary>
+        /// In-memory cached life information.
+        /// If null, it has not been fetched yet.
+        /// </summary>
         private Life? cachedLife = null;
+
+        /// <summary>
+        /// In-memory list of found content publications.
+        /// If null, it has not been fetched yet.
+        /// </summary>
+        private List<MyLife.Core.Models.ContentCreation.AccountPuplications>? contentPublications = null;
 
         #endregion
 
@@ -31,20 +43,42 @@ namespace MyLife.Core.Services
             return cachedLife;
         }
 
+        public async Task<List<AccountPuplications>?> GetContentPublications()
+        {
+            contentPublications ??= await FetchContentPublicationsFromServer();
+            return contentPublications;
+        }
+
+        #endregion
+
+        #region Private helpers
+
         /// <summary>
         /// Fetches the life information from a remote server.
+        /// 
+        /// If the services is mocked, it will return a sample data.
         /// </summary>
         /// <returns>The fetched life information.</returns>
         private async Task<Life?> FetchLifeFromServer()
         {
-            if (useRemote)
+            if (mocked)
             {
-                var json = await httpClient.GetStringAsync("data/life.json");
-                cachedLife = JsonConvert.DeserializeObject<Life>(json);
-                return cachedLife;
+                return SampleGenerators.GenerateLife();
             }
 
-            return SampleGenerators.GenerateLife();
+            var json = await httpClient.GetStringAsync(Constants.LifeJsonPath);
+            return JsonConvert.DeserializeObject<Life>(json);
+        }
+
+        private async Task<List<AccountPuplications>?> FetchContentPublicationsFromServer()
+        {
+            if (mocked)
+            {
+                return [];
+            }
+
+            var json = await httpClient.GetStringAsync(Constants.ContentCreationPublicationsPath);
+            return JsonConvert.DeserializeObject<List<AccountPuplications>>(json);
         }
 
         #endregion
