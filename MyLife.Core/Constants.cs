776bf8c19d6b9b643f173705b85546a7b122d1cb
Tobias Scholze <tobias.scholze@outlook.com>
2024-05-18 19:32:12 +0200

    [Core] Introduced core constants file

---
+++ MyLife.Core/Constants.cs
@@ -0,0 +1,20 @@
+﻿namespace MyLife.Core
+{
+    /// <summary>
+    /// Represents a summary of app-wide used constants.
+    /// </summary>
+    public class Constants
+    {
+        /// <summary>
+        /// URL path to the life information JSON file.
+        /// Default: "data/life.json"
+        /// </summary>
+        public const string LifeJsonPath = "data/life.json";
+
+        /// <summary>
+        /// URL path to the content creation publications JSON file.
+        /// Default: "data/cc-publications.json"
+        /// </summary>
+        public const string ContentCreationPublicationsPath = "data/cc-publications.json";
+    }
+}
