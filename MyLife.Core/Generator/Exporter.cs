317f6a2091e79b3497dc3e93d7cbe71f1fcdf89c
Tobias Scholze <tobias.scholze+git@me.com>
2024-07-02 19:38:24 +0200

    Linted

--- MyLife.Core/Generator/Exporter.cs
+++ MyLife.Core/Generator/Exporter.cs
@@ -1,19 +1,20 @@
 ﻿using MyLife.Core.Models;
 using MyLife.Core.Models.ContentCreation;
+using MyLife.Core.Models.Shared;
 using MyLife.Core.Services;
 using Newtonsoft.Json;
 
 namespace MyLife.Core;
 
 /// <summary>
-/// Life exporter.
+///     Life exporter.
 /// </summary>
 public static class Exporter
 {
     #region Public exporters
 
     /// <summary>
-    /// Export a life to a JSON string.
+    ///     Export a life to a JSON string.
     /// </summary>
     /// <param name="life">Life to convert</param>
     /// <returns>Resulting JSON string</returns>
@@ -23,16 +24,16 @@ public static class Exporter
     }
 
     /// <summary>
-    /// Export the latest publications of content feedable content creation accounts.
+    ///     Export the latest publications of content feedable content creation accounts.
     /// </summary>
     /// <param name="contentCreation">Container of accounts that shall be used.</param>
     /// <param name="verbose">If true, Console.Write will be used to inform the user what happens.</param>
     /// <returns>Resulting JSON string</returns>
-    public static async Task<string> ExportPublicationsAsync(Models.ContentCreation.Container contentCreation, bool verbose = true)
+    public static async Task<string> ExportPublicationsAsync(Container contentCreation, bool verbose = true)
     {
         var mediumService = new MediumService(new HttpClient());
         var kotlogService = new KotlogService(new HttpClient());
-        var accountPublications = new List<AccountPuplications>();
+        var accountPublications = new List<AccountPublications>();
 
         foreach (var account in contentCreation.Accounts)
         {
@@ -40,31 +41,28 @@ public static class Exporter
 
             switch (account.Platform)
             {
-                case Models.Shared.Platform.Youtube:
+                case Platform.Youtube:
                     publications = await YouTubeService.LoadPublicationsAsync(account.Handle);
                     break;
 
-                case Models.Shared.Platform.Medium:
+                case Platform.Medium:
                     publications = await mediumService.LoadPublicationsAsync(account.Handle);
                     break;
-                case Models.Shared.Platform.Kotlog:
+                case Platform.Kotlog:
                     publications = await kotlogService.LoadPublicationsAsync(account.Url.ToString());
                     break;
 
                 default:
-                    Console.WriteLine($"Platform feed loading not supported, the following account will be ignored: '{account.Handle}' on '{account.Platform}'");
+                    Console.WriteLine(
+                        $"Platform feed loading not supported, the following account will be ignored: '{account.Handle}' on '{account.Platform}'");
                     break;
             }
 
-            var accountPuplications = new AccountPuplications
+            accountPublications.Add(new AccountPublications
             {
                 Account = account,
                 Publications = publications
-            };
-
-
-
-            accountPublications.Add(accountPuplications);
+            });
         }
 
         if (verbose)
@@ -72,9 +70,8 @@ public static class Exporter
             var publicationCount = accountPublications.SelectMany(a => a.Publications).Count();
 
             Console.WriteLine($"""
-
-            Exported '{accountPublications.Count}' supported accounts with in total {publicationCount} publications.
-            """.Trim());
+                               Exported '{accountPublications.Count}' supported accounts with in total {publicationCount} publications.
+                               """.Trim());
         }
 
         return JsonConvert.SerializeObject(accountPublications);
